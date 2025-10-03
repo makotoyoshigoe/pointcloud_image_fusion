e69cc8e00063978942f364346e145dddf12d3dee
makotoyoshigoe <myoshigo0127@gmail.com>
2025-10-03 19:14:27 +0900

    add: jazzy test

--- test/setup.bash
+++ test/setup.bash
@@ -12,5 +12,5 @@ sudo make install
 # realsense packages
 cd $HOME
 sudo apt update
-sudo apt install -y ros-humble-realsense2-camera ros-humble-realsense2-camera-msgs ros-humble-realsense2-description
+sudo apt install -y ros-${ROS_DISTRO}-realsense2-camera ros-${ROS_DISTRO}-realsense2-camera-msgs ros-${ROS_DISTRO}-realsense2-description
 
